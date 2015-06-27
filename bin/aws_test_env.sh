fee52760cf8a486d651aafc33a729be51c79c637
AlanLiu-AI <alan.liu@aquaticinformatics.com>
2015-06-27 00:50:57 -0700

    down aws get metric period from one hour to ten minutes

--- bin/aws_test_env.sh
+++ bin/aws_test_env.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-date_onehourago() {
-	echo `date +%Y-%m-%dT%H:%M:%S -d  "1 hour ago"`
+date_tenminutesago() {
+	echo `date +%Y-%m-%dT%H:%M:%S -d "10 minute ago"`
 }
 date_now() {
         echo `date +%Y-%m-%dT%H:%M:%S`
 }
 
-alias date_begin=date_onehourago
+alias date_begin=date_tenminutesago
 alias date_end=date_now
 
 getElbHealthyHostCount() {
         elbIdentifier=$1
         statsMethod=$2
-        aws cloudwatch get-metric-statistics --start-time ${date_begin} --end-time ${date_end} --period 3600 --namespace 'AWS/ELB' --metric-name 'HealthyHostCount' --statistics $statsMethod --dimensions Name=LoadBalancerName,Value=$elbIdentifier
+        aws cloudwatch get-metric-statistics --start-time ${date_begin} --end-time ${date_end} --period 60 --namespace 'AWS/ELB' --metric-name 'HealthyHostCount' --statistics $statsMethod --dimensions Name=LoadBalancerName,Value=$elbIdentifier
 }
 getRdsConnectionCount() {
         rdsIdentifier=$1
         statsMethod=$2
-        aws cloudwatch get-metric-statistics --start-time $(date_begin) --end-time $(date_end) --period 3600 --namespace 'AWS/RDS' --metric-name 'DatabaseConnections' --statistics Minimum --dimensions Name=DBInstanceIdentifier,Value=$rdsIdentifier
+        aws cloudwatch get-metric-statistics --start-time $(date_begin) --end-time $(date_end) --period 3600 --namespace 'AWS/RDS' --metric-name 'DatabaseConnections' --statistics $statsMethod --dimensions Name=DBInstanceIdentifier,Value=$rdsIdentifier
 }
-
