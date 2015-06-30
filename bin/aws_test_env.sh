f5e8ca21cdfdda2db9c56c7d355c603fab8b8946
AlanLiu-AI <alan.liu@aquaticinformatics.com>
2015-06-29 17:45:33 -0700

    EnhancedTime

--- bin/aws_test_env.sh
+++ bin/aws_test_env.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 date_tenminutesago() {
-	echo `date +%Y-%m-%dT%H:%M:%S -d "10 minute ago"`
+        echo `date +%Y-%m-%dT%H:%M:%S -d "10 minute ago"`
+}
+date_fiveminutesago() {
+        echo `date +%Y-%m-%dT%H:%M:%S -d "5 minute ago"`
+}
+date_twominutesago() {
+        echo `date +%Y-%m-%dT%H:%M:%S -d "2 minute ago"`
+}
+date_oneminuteafter() {
+        echo `date +%Y-%m-%dT%H:%M:%S -d "1 minute"`
 }
 date_now() {
         echo `date +%Y-%m-%dT%H:%M:%S`
 }
 
-alias date_begin=date_tenminutesago
-alias date_end=date_now
-
 getElbHealthyHostCount() {
         elbIdentifier=$1
         statsMethod=$2
-        aws cloudwatch get-metric-statistics --start-time ${date_begin} --end-time ${date_end} --period 60 --namespace 'AWS/ELB' --metric-name 'HealthyHostCount' --statistics $statsMethod --dimensions Name=LoadBalancerName,Value=$elbIdentifier
+        CMD="aws cloudwatch get-metric-statistics --start-time $(date_twominutesago) --end-time $(date_oneminuteafter) --period 60 --namespace 'AWS/ELB' --metric-name 'HealthyHostCount' --statistics $statsMethod --dimensions Name=LoadBalancerName,Value=$elbIdentifier"
+        echo $CMD; eval $CMD
 }
 getRdsConnectionCount() {
         rdsIdentifier=$1
         statsMethod=$2
-        aws cloudwatch get-metric-statistics --start-time $(date_begin) --end-time $(date_end) --period 3600 --namespace 'AWS/RDS' --metric-name 'DatabaseConnections' --statistics $statsMethod --dimensions Name=DBInstanceIdentifier,Value=$rdsIdentifier
+        CMD="aws cloudwatch get-metric-statistics --start-time $(date_tenminutesago) --end-time $(date_oneminuteafter) --period 60 --namespace 'AWS/RDS' --metric-name 'DatabaseConnections' --statistics $statsMethod --dimensions Name=DBInstanceIdentifier,Value=$rdsIdentifier"
+        echo $CMD; eval $CMD
 }
